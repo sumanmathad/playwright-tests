00afe49e0f858516d9b5e2019ddae36c529a0831
suman_mathad <suman.mathad@gmail.com>
2023-03-10 19:52:41 +0530

    added login form validation test

--- playwright.config.js
+++ playwright.config.js
@@ -34,21 +34,23 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
-    actionTimeout: 0,
+    //actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
-
+    browserName: 'chromium',
+    headless:false
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+   // trace: 'on-first-retry',
+    
   },
 
   /* Configure projects for major browsers */
-  projects: [
+  /*projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     }
-    /*,
+    ,
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
@@ -78,7 +80,7 @@ module.exports = defineConfig({
     //   name: 'Google Chrome',
     //   use: { channel: 'chrome' },
     // },
-  ],
+  //],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
   // outputDir: 'test-results/',
--- tests/UIBasic.spec.js
+++ tests/UIBasic.spec.js
@@ -1,8 +1,27 @@
-const {test} = require('@playwright/test');
+const {test, expect} = require('@playwright/test');
 
-test('Launch browser and Navigate to test page', async ({browser, page})=>{
-   // const context= await browser.newContext();
-   // const page = await context.newPage();
+test('Launch browser and Navigate to test page through default context', async ({page})=>{
    await page.goto('https://the-internet.herokuapp.com/');
 });
+
+test('Launch Browser through new browser context',async({browser})=>{
+   const context = await browser.newContext();
+   const page = await context.newPage();
+
+   await page.goto('https://google.co.in');
+   await expect(page).toHaveTitle('Google');
+});
+
+test.only('Login to app and verify the landing page', async({page})=>{
+   await page.goto('https://the-internet.herokuapp.com/');
+   await expect(page).toHaveTitle('The Internet');
+   await page.locator("a[href='/login']").click();
+   await page.locator("#username").type("tomsmith");
+   await page.locator("#password").type("SuperSecretPassword!");
+   await page.locator(".fa").click();
+   await expect(page.locator("#content > div > h2")).toContainText("Secure Area");
+   
+});
+
+
 
